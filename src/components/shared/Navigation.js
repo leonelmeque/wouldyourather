6f2adc8eada4d6fe41b941d56b0bf4490fe1ffae
Leonel Meque <leomeque@gmail.com>
2020-06-26 08:17:35 +0200

    feat: navigation has been styled

--- src/components/shared/Navigation.js
+++ src/components/shared/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { authenticateUSer } from "../../actions/authUser";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./shared.css";
 
 /**
@@ -11,31 +12,52 @@ class Navigation extends React.Component {
   render() {
     const { dispatch, users } = this.props;
     return (
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/add">New Question</Link>
-        <Link to="/leaderboard">Leader Board</Link>
-
-        {this.props.authUser.id && (
-          <span
-            style={{
-              margin:'0px 10px'
-            }}
-          >Hello {users[this.props.authUser.id].name}</span>
-        )}
-        <img src="" alt="" />
-        {this.props.authUser.id && (
-          <Link
-            to="/login"
-            onClick={(e) => {
-              e.stopPropagation();
-              dispatch(authenticateUSer(null));
-            }}
-          >
-            Logout
-          </Link>
-        )}
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">Would Your Rather</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse mb="mr-auto" className="justify-content-end">
+            <Nav>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              <Link className="nav-link" to="/add">
+                New Question
+              </Link>
+              <Link className="nav-link" to="/leaderboard">
+                Leader Board
+              </Link>
+              {this.props.authUser.id && (
+                <span
+                  style={{
+                    margin: "auto 10px",
+                    color:"#ffffff"
+                  }}
+                >
+                  Hi{" "}
+                  <span role="img" aria-label="Waving Hand">
+                    👋
+                  </span>{" "}
+                  {users[this.props.authUser.id].name}
+                </span>
+              )}
+              <img src="" alt="" />
+              {this.props.authUser.id && (
+                <Link
+                  className="nav-link"
+                  to="/login"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    dispatch(authenticateUSer(null));
+                  }}
+                >
+                  Logout
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
   }
 }
