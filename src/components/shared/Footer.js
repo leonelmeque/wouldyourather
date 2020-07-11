24523164b41be23a769bb62845c6ac0380208c04
mequedev <leomeque@gmail.com>
2020-07-11 11:34:28 +0200

    feat: added Footer + fix to container high

---
+++ src/components/shared/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./shared.css";
+
+export default function Footer(){
+    return(
+        <footer>
+            <p>Would You Rather</p>
+        </footer>
+    )
+}
